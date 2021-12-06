d931bcf7e2bf9cbd7b3f6e6335ed1c0fbed40a3c
veselovvv <vycheslav5veselov@gmail.com>
2021-12-06 10:33:34 +0200

    Added NewMessageActivity and LastMessagesActivity

---
+++ app/src/main/java/com/veselovvv/messenger/NewMessageActivity.kt
@@ -0,0 +1,14 @@
+package com.veselovvv.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_message)
+
+        supportActionBar?.title = "Select User"
+    }
+}
