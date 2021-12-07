1f052d88f112835ca7d5241b18c50bdc1678e361
veselovvv <vycheslav5veselov@gmail.com>
2021-12-07 15:38:24 +0200

    Added user photos to the user list

--- app/src/main/java/com/veselovvv/messenger/NewMessageActivity.kt
+++ app/src/main/java/com/veselovvv/messenger/NewMessageActivity.kt
@@ -7,6 +7,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
@@ -61,6 +62,8 @@ class NewMessageActivity : AppCompatActivity() {
 class UserItem(val user: User) : Item<GroupieViewHolder>() {
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         viewHolder.itemView.username_textview_new_message.text = user.username
+
+        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.imageview_new_message)
     }
 
     override fun getLayout(): Int {
