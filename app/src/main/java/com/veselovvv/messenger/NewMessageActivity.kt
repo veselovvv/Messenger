869df2615ab77cc9baa8d250ab05e1aa02c55962
Viacheslav Veselov <vycheslav5veselov@gmail.com>
2021-12-08 18:36:29 +0200

    Update NewMessageActivity.kt

--- app/src/main/java/com/veselovvv/messenger/NewMessageActivity.kt
+++ app/src/main/java/com/veselovvv/messenger/NewMessageActivity.kt
@@ -30,7 +30,7 @@ class NewMessageActivity : AppCompatActivity() {
 
     private fun fetchUsers() {
         val ref = FirebaseDatabase
-            .getInstance("https://messenger-79c50-default-rtdb.europe-west1.firebasedatabase.app/")
+            .getInstance("YourDatabaseLink")
             .getReference("/users")
 
         ref.addListenerForSingleValueEvent(object : ValueEventListener {
